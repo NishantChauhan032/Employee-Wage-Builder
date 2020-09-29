5d2c59dc28b9f301269ca6157d96a80a6018811f
NishantChauhan032 <nishantkr0210@gmail.com>
2020-09-29 16:04:16 +0530

    PART TIME EMPLOYEE DAILY WAGE CALCULATION ADDED

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,12 +3,15 @@ public class EmpWageBuilder{
      System.out.println("Welcome to Employee Wage Computation Program");
       
       int isFullTime = 1;
+      int isPartTime = 2; 
       int empRatePerHour = 20;
       int empHours = 0;
       int empWage = 0;
-      double attendence = Math.floor(Math.random()*10) % 2;
+      double attendence = Math.floor(Math.random()*10) % 3;
          if(attendence == isFullTime)
               empHours = 8;
+         else if(attendence == isPartTime)
+              empHours = 4;
          
          empWage = empHours*empRatePerHour;
          System.out.println("Daily Wage Of Employee Is : " + empWage);
