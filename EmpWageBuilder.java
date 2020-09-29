4dc820c28f7b4f4c5c83fa9ed35e21823787efe9
NishantChauhan032 <nishantkr0210@gmail.com>
2020-09-29 16:14:46 +0530

    EMPLOYEE WAGE CALCULATED USING SWITCH

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,19 +1,26 @@
 public class EmpWageBuilder{
+    public static final int IS_PART_TIME=2;
+    public static final int IS_FULL_TIME=1;
+    public static final int RATE_PER_HOUR=20;
+
    public static void main(String[] args){
      System.out.println("Welcome to Employee Wage Computation Program");
-      
-      int isFullTime = 1;
-      int isPartTime = 2; 
-      int empRatePerHour = 20;
-      int empHours = 0;
-      int empWage = 0;
+
+      int empHours=0;
+      int empWage=0;
       double attendence = Math.floor(Math.random()*10) % 3;
-         if(attendence == isFullTime)
-              empHours = 8;
-         else if(attendence == isPartTime)
-              empHours = 4;
-         
-         empWage = empHours*empRatePerHour;
+       switch ((int)attendence){
+         case IS_FULL_TIME:
+            empHours=8;
+            break;
+         case IS_PART_TIME:
+            empHours=4;
+            break;
+         default :
+            empHours=0;
+         }
+ 
+         empWage = empHours * RATE_PER_HOUR;
          System.out.println("Daily Wage Of Employee Is : " + empWage);
    }
  }
